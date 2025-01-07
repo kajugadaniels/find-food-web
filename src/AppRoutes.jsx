a6702f7de88cde9ac2d1f60adf654ae069d57a28
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-07 10:56:32 +0200

    Setup routes

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import PageLayout from './layouts/PageLayout'
+import { Route, Routes } from 'react-router-dom'
+import { Home, NotFound } from './pages'
 
 const AppRoutes = () => {
     return (
-        <div>AppRoutes</div>
+        <Routes>
+            <Route element={<PageLayout />}>
+                <Route path="/" element={<Home />} />
+
+                <Route path="*" element={<NotFound />} />
+            </Route>
+        </Routes>
     )
 }
 
--- src/layouts/PageLayout.jsx
+++ src/layouts/PageLayout.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const PageLayout = () => {
     return (
-        <div>PageLayout</div>
+        <div>
+            PageLayout
+            <Outlet />
+        </div>
     )
 }
 
