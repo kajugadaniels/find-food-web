b34e6fe23a21eda8b49dda5a5cc13a9757942bd7
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-13 10:21:06 +0200

    New changes

--- src/layouts/PageLayout.jsx
+++ src/layouts/PageLayout.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { Header } from '../components'
 
 const PageLayout = () => {
     return (
-        <div>
-            PageLayout
-            <Outlet />
+        <div id="wrapper">
+            <div id="pagee" className="clearfix">
+                <Header />
+                PageLayout
+                <Outlet />
+            </div>
         </div>
     )
 }
