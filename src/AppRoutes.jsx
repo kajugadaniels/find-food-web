8cf31add1f458d3ddb442efc174d15f61531eb14
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-13 10:22:21 +0200

    Setup places page

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import PageLayout from './layouts/PageLayout'
 import { Route, Routes } from 'react-router-dom'
-import { Home, NotFound } from './pages'
+import { Home, NotFound, Places } from './pages'
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route element={<PageLayout />}>
-                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/" element={<Places />} />
 
                 <Route path="*" element={<NotFound />} />
             </Route>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 export { default as Home } from './Home'
+export { default as Places } from './Places'
 export { default as NotFound } from './NotFound'
