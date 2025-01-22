d9f9e103f3f12742d1d4a641608a10a6ad56bf47
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-23 01:07:10 +0200

    Design header

--- src/layouts/PageLayout.jsx
+++ src/layouts/PageLayout.jsx
@@ -4,8 +4,23 @@ import { Header } from '../components'
 
 const PageLayout = () => {
     return (
-        <div id="wrapper">
-            <div id="pagee" className="clearfix">
+        <div className='homepage1-body'>
+            <div>
+                <div class="preloader">
+                    <div class="loading-container">
+                        <div class="loading"></div>
+                        <div id="loading-icon"><img src="https://housebox-html-demo.vercel.app/assets/img/logo/preloader.png" alt="housebox" /></div>
+                    </div>
+                </div>
+
+                <div class="paginacontainer">
+                    <div class="progress-wrap">
+                        <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+                            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+                        </svg>
+                    </div>
+                </div>
+
                 <Header />
                 PageLayout
                 <Outlet />
