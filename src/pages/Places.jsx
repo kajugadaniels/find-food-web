3a54531c4731ab1b19ec963db0ad0eb39de87920
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-23 02:00:00 +0200

    Add searchFilter

--- src/pages/Places.jsx
+++ src/pages/Places.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { SearchFilter } from '../components'
 
 const Places = () => {
     return (
-        <div>Places</div>
+        <div className="hero-inner-section-area grid-area">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/7/78/KCC_Wallpaper_by_Mudahunga.jpg" alt="housebox" className="hero-img1" />
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="hero-header-area text-center">
+                            <h1>Good Place to Taste Their Food</h1>
+                        </div>
+                        <div className="space80"></div>
+                    </div>
+                </div>
+            </div>
+
+            <SearchFilter />
+        </div>
     )
 }
 
